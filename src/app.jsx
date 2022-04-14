f5814665a833e0bab5b13ac511bfb68583e4e28e
a10x <arthatixarca@gmail.com>
2022-04-14 04:21:32 +0100

    Initialise Neutralinojs along with Vite and Preact

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+export function App(props) {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+
+    </>
+  )
+}
